2332fed568d24656f99babaefa4187a6faba9e56
felipeam10 <felipeam10@hotmail.com>
2021-09-01 20:21:59 -0300

    v2.6 first test

--- src/main/java/philips/rest/HelloWord.java
+++ src/main/java/philips/rest/HelloWord.java
@@ -3,11 +3,15 @@ package philips.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class HelloWord {
     public static void main(String[] args) {
         Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-        System.out.println(response.getBody().asString());
-        System.out.println(response.statusCode());
+        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+        System.out.println(response.statusCode() == 200);
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
     }
 }
