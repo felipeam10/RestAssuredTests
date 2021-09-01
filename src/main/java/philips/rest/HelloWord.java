7862e55c6ae65200ddefa5dbcb6196e636cb1f67
felipeam10 <felipeam10@hotmail.com>
2021-09-01 20:07:46 -0300

    v2.5 get status code

--- src/main/java/philips/rest/HelloWord.java
+++ src/main/java/philips/rest/HelloWord.java
@@ -8,5 +8,6 @@ public class HelloWord {
     public static void main(String[] args) {
         Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
         System.out.println(response.getBody().asString());
+        System.out.println(response.statusCode());
     }
 }
