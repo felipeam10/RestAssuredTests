b096b5d0ee9afcc82d3557dcbf34fdc8675e8b95
felipeam10 <felipeam10@hotmail.com>
2021-09-01 21:20:50 -0300

    v2.8 modo fluente

--- src/main/java/philips/rest/HelloWordTest.java
+++ src/main/java/philips/rest/HelloWordTest.java
@@ -7,17 +7,31 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+
 public class HelloWordTest {
 
     @Test
     public void testHelloWord() {
-        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
         Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
         Assert.assertTrue(response.statusCode() == 200);
         Assert.assertTrue("O status code deveria ser 200", response.statusCode() == 200);
-        Assert.assertEquals(201, response.statusCode());
+        Assert.assertEquals(200, response.statusCode());
 
         ValidatableResponse validacao = response.then();
         validacao.statusCode(200);
+    }
+
+    @Test
+    public void modoFluenteWithRestAssured() {
+        get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+
+//      modo fluente
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/ola")
+        .then()
+            .statusCode(200);
     }
 }
