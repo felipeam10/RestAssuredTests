28f486a933ec4fb994ffeb3adb2672c956215da3
felipeam10 <felipeam10@hotmail.com>
2021-09-04 01:46:19 -0300

    v3.11 json primeiro nivel

---
+++ src/main/java/philips/rest/UserJsonTest.java
@@ -0,0 +1,45 @@
+package philips.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.nullValue;
+
+public class UserJsonTest {
+
+    @Test
+    public  void verificandoPrimeiroNivel() {
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/users/1")
+        .then()
+            .statusCode(200)
+            .body("id", is(1))
+            .body("name", containsString("Silva"))
+            .body("age", greaterThan(18))
+        ;
+    }
+
+    @Test
+    public void verificandoPrimeiroNivelOutrasFormas() {
+        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/users/1");
+
+        //path
+        Assert.assertEquals(new Integer(1), response.path("id"));
+        Assert.assertEquals(new Integer(1), response.path("%s", "id"));
+
+        //jsonPath
+        JsonPath jsonPath = new JsonPath(response.asString());
+        Assert.assertEquals(1, jsonPath.getInt("id"));
+
+        //from
+        int id = JsonPath.from(response.asString()).getInt("id");
+        Assert.assertEquals(1, id);
+    }
+}
