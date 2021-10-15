ea5de4bd83d76140e1539fec029141ab05638bfa
felipeam10 <felipeam10@hotmail.com>
2021-10-15 11:37:27 -0300

    v4.18 utilizando xml

--- src/main/java/philips/rest/UserXMLTest.java
+++ src/main/java/philips/rest/UserXMLTest.java
@@ -1,2 +1,26 @@
-package philips.rest;public class UserXMLTest {
+package philips.rest;
+
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class UserXMLTest {
+
+    @Test
+    public void trabalhandoComXml(){
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/usersXML/3")
+        .then()
+            .statusCode(200)
+            .body("user.name", is("Ana Julia"))
+            .body("user.@id", is("3"))
+            .body("user.filhos.name.size()", is(2))
+            .body("user.filhos.name[0]", is("Zezinho"))
+            .body("user.filhos.name[1]", is("Luizinho"))
+            .body("user.filhos.name", hasItem("Luizinho"))
+            .body("user.filhos.name", hasItems("Luizinho", "Zezinho"))
+        ;
+    }
 }
