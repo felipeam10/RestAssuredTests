965a3213e12503e1f4050434463a745b11c688b3
felipeam10 <felipeam10@hotmail.com>
2022-10-27 01:14:48 -0300

    upload de arquivos

--- src/main/java/philips/rest/FileTest.java
+++ src/main/java/philips/rest/FileTest.java
@@ -1,2 +1,40 @@
-package philips.rest;public class FileTest {
+package philips.rest;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class FileTest {
+
+    @Test
+    public void deveObrigarEnvioDoArquivo(){
+        given()
+                .log().all()
+        .when()
+                .post("https://restapi.wcaquino.me/upload")
+        .then()
+                .log().all()
+                .statusCode(404)
+                .body("error", Matchers.is("Arquivo não enviado"))
+        ;
+    }
+
+    @Test
+    public void deveFazerUploadDoArquivo(){
+        given()
+                .log().all()
+                .multiPart("arquivo", new File("src/main/resources/users.pdf"))
+        .when()
+                .post("https://restapi.wcaquino.me/upload")
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("users.pdf"))
+        ;
+    }
 }
