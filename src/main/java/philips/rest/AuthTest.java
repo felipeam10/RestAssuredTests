db7e1612ff6cbd299208bf2a0b4aa2161e6c5e0c
felipeam10 <felipeam10@hotmail.com>
2022-12-30 00:44:35 -0300

    acessando API pública

---
+++ src/main/java/philips/rest/AuthTest.java
@@ -0,0 +1,31 @@
+package philips.rest;
+
+import io.restassured.http.ContentType;
+import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.xml.sax.SAXParseException;
+
+import java.io.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class AuthTest {
+
+    @Test
+    public void deveAcessarSWAPI(){
+        given()
+                .log().all()
+        .when()
+                .get("https://swapi.dev/api/people/1/")
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Luke Skywalker"))
+        ;
+
+    }
+}
